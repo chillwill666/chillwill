0bf7440db08b3a18dc11a24aa5353262cecdbc13
Charlie Redden <iam@charlieredden.com>
2015-08-14 04:24:57 -0700

    Moved old stuff into old folder, uploading new stuff

---
+++ shitwillsez.js
@@ -0,0 +1,18 @@
+var quotes = [
+                    'Try to see life as it is, not as it should be',
+                    'Chill or be Chilled',
+                    'What kinda phone you got?',
+                    'I go to micky dees n\' get an english mc muffin',
+                    'Yea, I\'m throwin D\'s on the Cadillac',
+                    'Gag a bitch and shove her in the dryer at the laundry mat',
+                    'You niggas fake like mall cop, Paul Blart I run you over with the shopping cart in Wal- Mart',
+                    'And spend a hundred grand on a one-legged prostitute',
+                    'pokahs',
+                    'I hop freight',
+                    'oh you know... pickin\' it..',
+                    'What kinda phone yo girl got?',
+                    'fuckin\' retahded'
+                ];
+                var length = quotes.length;
+                var rand = Math.round(Math.random()*(length - 1));
+                document.write(quotes[rand]);
